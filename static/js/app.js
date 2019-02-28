e5c8797008d9cd2329ba02e8c008c839cd148e88
Tony Li <tli415@gmail.com>
2019-02-28 11:36:28 -0800

    add grid styles

--- static/js/app.js
+++ static/js/app.js
@@ -421,13 +421,7 @@ var epm_container = {};
 function update_epm(epm, svg_area) {
     if (!epm_text) {
         epm_container = svg_area.append('g')
-            .attr('transform', 'translate(0, ' + (height - 25) + ')');
-
-        var epm_box = epm_container.append('rect')
-            .attr('fill', newuser_box_color)
-            .attr('opacity', 0.5)
-            .attr('width', 135)
-            .attr('height', 25);
+            .attr('transform', 'translate(0, ' + (height - 25) + ')')
 
         epm_text = epm_container.append('text')
             .classed('newuser-label', true)
